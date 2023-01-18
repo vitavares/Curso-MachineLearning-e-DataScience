e1be91f055aff441ac58e0be37d6797f77bdc843
Vitória Tavares <vitoriadesousat@gmail.com>
2023-01-18 19:00:47 -0300

    Resolução de exercicios

--- Exercícios/ex4.py
+++ Exercícios/ex4.py
@@ -6,3 +6,18 @@
 # - Se a média for maior do que 6.0, o aluno está aprovado
 # - Se o aluno pegou exame, deve ser lida a nota do exame. Se a nota do exame for maior do que 6.0, está aprovado, senão; está reprovado
 
+m1 = float(input('Nota M1: '))
+m2 = float(input('Nota M2: '))
+m3 = float(input('Nota M3: '))
+
+media = (m1+m2+m3)/3
+
+if media>=0.0 and media<=4.0:
+    print('\nO aluno está reprovado.')
+elif media>=4.1 and media<=6.0:
+    print('\nO aluno pegou exame.')
+    n_exame = float(input('\nNota do exame: '))
+    res = 'aprovado' if n_exame>6.0 else 'reprovado'
+    print('\nO aluno está', res)
+elif media>6.0:
+    print('\nO aluno está aprovado.')
